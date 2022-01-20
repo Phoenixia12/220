9c8dccf6e019bbe14710d45af97d44e2193bca70
Phoenixia12 <aeidenberger02@gmail.com>
2022-01-20 15:00:32 -0500

    completed homework 1

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,34 +1,54 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Ashley Eidenberger
+hw1.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: The following programs are fairly simple, the user
+inputs a set of values, and using algebra, gives the user a
+result
 
 Certification of Authenticity:
-<include one of the following>
+
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+
 """
 
 
 def calc_rec_area():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area =", area)
+
+
 
 
 def calc_volume():
-    pass
+    length_volume = eval(input("Enter the length: "))
+    width_volume = eval(input("Enter the width: "))
+    height_volume = eval(input("Enter the height: "))
+    volume = length_volume * width_volume * height_volume
+    print("Volume =", volume)
 
 
 def shooting_percentage():
-    pass
+    total_shot = eval(input("Enter the player's total shots: "))
+    shots_made = eval(input("Enter how many shots the player made: "))
+    percentage = (shots_made / total_shot) * 100
+    print("Shooting Percentage:", percentage,"%")
+
 
 
 def coffee():
-    pass
+    pounds = eval(input("How many pounds of coffee would you like"))
+    total = (10.50 * pounds) + (0.86 * pounds) + 1.50
+    print("Your total is: $", total)
+
 
 
 def kilometers_to_miles():
-    pass
+    kilo = eval(input("How many kilometers did you travel?: "))
+    mile = kilo / 1.61
+    print("That's", mile, "miles!")
 
 
 if __name__ == '__main__':
